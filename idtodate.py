3298d4c15100b7be02a5945d0b90e474e96ac3a8
cChimney <66905361+cChimney@users.noreply.github.com>
2022-05-01 16:29:34 +0200

    Add files via upload

---
+++ idtodate.py
@@ -0,0 +1,37 @@
+from py_mini_racer import py_mini_racer
+import time, datetime
+ctx = py_mini_racer.MiniRacer()
+
+def checkDate(id):
+    script = """
+    function convert(id=%s) {
+        var bin = (+id.toString()).toString(2);
+        var unixbin = '';
+        var unix = '';
+        var m = 64 - bin.length;
+        unixbin = bin.substring(0, 42-m);
+        unix = parseInt(unixbin, 2) + 1420070400000;
+        return unix;
+    }
+    """ % (id)
+    ctx.eval(script)
+    val = ctx.call("convert")
+    return val
+
+def worker(age):
+    Older = []
+    ids = open('ids.txt').read().splitlines()
+    unixAge = int(time.mktime(datetime.datetime.strptime(age, "%d/%m/%Y").timetuple()))*1000
+    for id in ids:
+        idAge = checkDate(id)
+        if int(unixAge) > int(idAge):
+            Older.append(id)
+    with open('olderIds.txt', 'a+') as f:
+        for id in Older:
+            f.write(f"{id}\n")
+
+try:
+    age = input("Save ids that are older than what date? Example 15/12/2015 (european dates)\n")
+    worker(age)
+except Exception as err:
+    print(err)
